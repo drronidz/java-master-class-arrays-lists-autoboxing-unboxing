ff016c340e4d2ab1d9c4d932cecbfb8d8f84804b
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-15 00:22:45 +0100

    issue done. closes #11.

--- ArrayListChallenge/src/MobilePhone.java
+++ ArrayListChallenge/src/MobilePhone.java
@@ -4,6 +4,11 @@ public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
 
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
     public MobilePhone(String myNumber, ArrayList<Contact> myContacts) {
         this.myNumber = myNumber;
         this.myContacts = myContacts;
@@ -28,6 +33,17 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean removeContact(Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition <0) {
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted");
+        return true;
+    }
+
     private int findContact(Contact contact) {
         return this.myContacts.indexOf(contact);
     }
@@ -41,4 +57,18 @@ public class MobilePhone {
         return -1;
     }
 
+    public String queryContact(Contact contact){
+        if(findContact(contact) >=0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for(int i=0; i<this.myContacts.size(); i++) {
+            System.out.println((i+1) + "." + this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
+    }
 }
